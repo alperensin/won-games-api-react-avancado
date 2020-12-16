545e11863ae30448860955d0237c4c7663360935
alperensin <andreluizperensin@hotmail.com>
2020-12-15 21:03:01 -0300

    Initial Commit - Setup Project > Create/Setup Database Local (Postgresql) | Setup CKEditor | Create collections: category, developer, game, platform, publisher.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df192acbaafa6bfd1f81329b27d502c3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77a1f829-091c-48c9-b8d9-bb9dc73ac1d1'
+};
